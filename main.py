1913782744ae6e99add31ff38971b93a138c59a2
Jude Adham <judeadham@gmail.com>
2020-12-24 23:31:36 -0500

    Organized code a bit more and fixed an error with an empty about section.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import requests
 
-if __name__ == '__main__':
+def getResponse():
     query = '''
-    query ($name: String) {
-        User (name: $name) {
-            name
-            about
-        }
-    }
-    '''
+       query ($name: String) {
+           User (name: $name) {
+               name
+               about
+           }
+       }
+       '''
 
     userName = input("Enter your Anilist username: ")
 
@@ -20,8 +20,21 @@ if __name__ == '__main__':
 
     response = requests.post(url, json={'query': query, 'variables': variables})
 
+    return response
+
+def printResponse(response):
     if response.status_code == 200:
         json_resp = response.json()
-        print('Your name is: ' + json_resp['data']['User']['name'] + '\nand your about is: ' + json_resp['data']['User']['about'])
+        if json_resp['data']['User']['about'] is None:
+            print("Could not find an about, but your name is: " + json_resp['data']['User']['name'])
+        else:
+            print('Your name is: ' + json_resp['data']['User']['name'] + '\nand your about is: ' + json_resp['data']['User']['about'])
     elif response.status_code == 404:
         print("Could not find your Anilist Profile.")
+
+def main():
+    response = getResponse()
+    printResponse(response)
+
+if __name__ == '__main__':
+    main()
